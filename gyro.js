8a548fe232df99b165d877633f265c570b6f83ed
kandarpdangi <51157743+kandarpdangi@users.noreply.github.com>
2020-12-11 21:50:50 +0530

    Add files via upload

--- gyro.js
+++ gyro.js
@@ -1,6 +1,8 @@
 
 
 function handleMotion(event) {
+  const windows = document.querySelector('.windows');
+  const room = document.querySelector(".room");
   console.log('-----------------');
   let gyro_z = event.rotationRate.alpha;
   let gyro_x = event.rotationRate.beta;
@@ -27,24 +29,11 @@ function gyros(e){
 
 const windows = document.querySelector('.windows');
 const room = document.querySelector(".room");
-const title = document.querySelector(".title");
-const profliepic = document.querySelector(".profilepic img");
-const tabs = document.querySelector(".tabs");
-const desc = document.querySelector(".about h4");
 
 room.addEventListener('mousemove',(e)=>{
     let xAxis = (window.innerWidth/2 -e.pageX)/10;
     let yAxis = (window.innerHeight/2 -e.pageY)/10;
     windows.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-
-    title.style.transform='translateZ(120px)';
-    profliepic.style.transform='translateZ(120px)'
-    desc.style.transform='translateZ(120px)';
-    title.style.background='linear-gradient(to right,rgba(163, 68, 218, 0.8),rgba(42, 196, 216, 0.8))';
-    title.style.color='pink'
-    tabs.style.transform='translateZ(120px)';
-    title.style.transform='translateZ(120px)';
-
 });
 
 room.addEventListener('mouseenter',(e)=>{
@@ -53,9 +42,4 @@ room.addEventListener('mouseenter',(e)=>{
 room.addEventListener('mouseleave',(e)=>{
     windows.style.transition="all 0.6s ease";
     windows.style.transform = `rotateY(0deg)`
-    title.style.transform='translateZ(0px)';
-    profliepic.style.transform='translateZ(0px) rotateZ(0deg)'
-    desc.style.transform='translateZ(0px)';
-    tabs.style.transform='translateZ(0px)';
-    title.style.transform='translateZ(0px)';
 });
